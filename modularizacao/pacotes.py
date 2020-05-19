01afa50cca1b81a72f7830281636ea8d76930d41
TaySabrina <taysabrina.prog@gmail.com>
2020-05-19 11:31:15 -0300

    adding exercise 110

---
+++ modularizacao/pacotes.py
@@ -0,0 +1,5 @@
+from uteis import numeros
+#importando modulos de pacotes
+
+p = float(input('Digite o preço: R$ '))
+numeros.resumo(p, 35, 22)
--- modularizacao/uteis/numeros/__init__.py
+++ modularizacao/uteis/numeros/__init__.py
@@ -5,23 +5,39 @@ def fatorial(n):
     return f
 
 
-def dobro(n = 0, formato = False):
+def dobro(n=0, formato=False):
     res = n * 2
     return res if not formato else moeda(res)
 
 
-def triplo(n = 0):
+def triplo(n=0):
     return n * 3
 
 
-def metade(n = 0, formato = False):
+def metade(n=0, formato=False):
     res = n / 2
     return res if not formato else moeda(res)
 
 
-def porc(n = 0, formato = False):
-    res = n + (n * 0.10)
+def porc(n=0, taxa=0, formato=False):
+    res = n + (n * taxa / 100)
     return res if formato is False else moeda(res)
 
-def moeda(preco = 0, moeda = 'R$'):
+
+def reducao(preco=0, taxa=0, formato=False):
+    res = preco - (preco * taxa / 100)
+    return res if formato is False else moeda(res)
+
+
+def moeda(preco=0, moeda='R$'):
     return f'{moeda}{preco:>.2f}'.replace('.', ',')
+
+def resumo(preco=0, taxaa=10, taxar=5 ):
+    print('-' * 30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-' * 30)
+    print(f'Preço analisado: \t{moeda(preco)}')
+    print(f'Dobro do preço: \t{dobro(preco, True)}')
+    print(f'Metade do preço: \t{metade(preco, True)}')
+    print(f'{taxaa}% de aumento: \t{porc(preco, taxaa, True)}')
+    print(f'{taxar}% de redução: \t{reducao(preco, taxar, True)}')

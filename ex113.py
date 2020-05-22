792aeffd794804802b542f2195299165a651755b
TaySabrina <taysabrina.prog@gmail.com>
2020-05-22 10:57:53 -0300

    adding exercise 113

---
+++ ex113.py
@@ -0,0 +1,31 @@
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: por favor, digite um número inteiro válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO:por favor digite um número real válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+
+ #Programa Principal
+num = leiaInt('Digite um número INTEIRO: ')
+num2 = leiaFloat('Digite um número REAL: ')
+print(f'O valor inteiro digitado foi {num} e o real foi {num2:.1f}')

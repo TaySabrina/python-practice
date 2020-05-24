aead19a2e21c285d8dec133f8ad12fa44b99ef91
TaySabrina <taysabrina.prog@gmail.com>
2020-05-24 15:00:42 -0300

    adding exercise 115c final

--- modularizacao/ex115/lib/arquivo/__init__.py
+++ modularizacao/ex115/lib/arquivo/__init__.py
@@ -26,4 +26,22 @@ def lerArquivo(nome):
         print('\033[31mErro ao ler o arquivo!\033[m')
     else:
         cabecalho('PESSOAS CADASTRADAS')
-        print(a.read())
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+    finally:
+        a.close()
+
+def cadastrar(arq, nome = 'desconhecido', idade = 0):
+    try:
+        a = open(arq, 'at')
+    except:
+        print('\033[31mHouve um erro na abertura do arquivo!\033[m')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n')
+        except:
+            print('\033[31mHouve um erro na hora de escrever os dados!\033[m')
+        else:
+            print(f'\033[34mNovo registro de {nome} adicionado.\033[m')
